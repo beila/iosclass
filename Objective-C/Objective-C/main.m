4fc4ce15a8574d850df73f003daee57e6c94b250
Hojin Ghim <hojin.ghim@lge.com>
2015-03-09 11:01:42 +0900

    hello obj-c

---
+++ Objective-C/Objective-C/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Objective-C
+//
+//  Created by Eizer-N39 on 2015. 3. 9..
+//  Copyright (c) 2015년 beila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    const char* s1 = "Hello C";
+    NSString* s2 = @"Hello Obj-C";
+    
+    printf("%s\n", s1);
+    NSLog(@"%@", s2);
+    
+    NSLog(@"%s %@", s1, s2);
+}
