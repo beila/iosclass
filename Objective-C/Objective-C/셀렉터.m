ba42050f5d2c95617430bf0c04d3e4fe4fa614b2
Hojin Ghim <hojin.ghim@lge.com>
2015-03-09 15:21:59 +0900

    hello target and action

--- Objective-C/Objective-C/셀렉터.m
+++ Objective-C/Objective-C/셀렉터.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015년 beila. All rights reserved.
 //
 
+#if 0
+
 #import <Foundation/Foundation.h>
 
 @interface Car: NSObject
@@ -50,3 +52,5 @@ int main() {
     F f2 = (F)[p methodForSelector:s2];
     f2(p, s2);
 }
+
+#endif
---
+++ Objective-C/Objective-C/타겟액션.m
@@ -0,0 +1,44 @@
+//
+//  타겟액션.m
+//  Objective-C
+//
+//  Created by Eizer-N39 on 2015. 3. 9..
+//  Copyright (c) 2015년 beila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dialog : NSObject
+- (void)close;
+@end
+
+@implementation Dialog
+- (void)close {NSLog(@"Dialog closed!!");}
+@end
+
+@interface Button : NSObject
+{
+    id target;
+    SEL action;
+}
+
+- (void)addTarget:(id)t action:(SEL)a;
+- (void)click;
+@end
+
+@implementation Button
+- (void)addTarget:(id)t action:(SEL)a {target = t; action = a;}
+- (void)click {[target performSelector:action];}
+@end
+
+int main() {
+    Dialog* dialog = [Dialog alloc];
+    Button* button1 = [Button alloc];
+    [button1 addTarget:dialog action:@selector(close)];
+    [button1 click];
+    
+    Button* button2 = [Button alloc];
+    [button2 click];
+    [button2 addTarget:dialog action:@selector(close)];
+    [button2 click];
+}
