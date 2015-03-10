4af9081020e8c8f38d87d04aec9cb1504e8fbfe3
Hojin Ghim <hojin.ghim@lge.com>
2015-03-10 09:14:19 +0900

    new project for second day

---
+++ Obj-C2/Obj-C2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Obj-C2
+//
+//  Created by Eizer-N39 on 2015. 3. 10..
+//  Copyright (c) 2015년 beila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
