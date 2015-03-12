43e9ab03ea806a1427fb18d49f4b5f513baf0de7
Hojin Ghim <hojin.ghim@lge.com>
2015-03-12 10:14:02 +0900

    hello child view

--- MultiView/MultiView/ViewController.m
+++ MultiView/MultiView/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UIView *childView;
+
+- (IBAction)click1:(id)sender;
+- (IBAction)click2:(id)sender;
+
 @end
 
 @implementation ViewController
@@ -20,4 +25,32 @@
     // Do any additional setup after loading the view from its nib.
 }
 
+- (IBAction)click1:(id)sender {
+    NSLog(@"%s", __func__);
+    
+    _childView.frame = CGRectMake(0,0,0,0);
+    _childView.transform = CGAffineTransformMakeRotation(3.14);
+    
+    [self.view insertSubview:_childView atIndex:0];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:0.5];
+    
+    _childView.frame = CGRectMake(0,0,320,480);
+    _childView.transform = CGAffineTransformMakeRotation(0);
+    
+    [UIView commitAnimations];
+
+}
+
+- (IBAction)click2:(id)sender {
+    NSLog(@"%s", __func__);
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1];
+    [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromRight forView:self.view cache:YES];
+    
+    [_childView removeFromSuperview];
+    
+    [UIView commitAnimations];
+}
 @end
