66d66194aa123d71ad081691c618a44332f45db5
Hojin Ghim <hojin.ghim@lge.com>
2015-03-12 16:34:23 +0900

    hello image picker for photo library and camera

--- Camera/Camera/ViewController.m
+++ Camera/Camera/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+- (IBAction)click1:(id)sender;
+- (IBAction)click2:(id)sender;
 
 @end
 
@@ -24,4 +27,32 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    UIImage* img1 = [info objectForKey:UIImagePickerControllerOriginalImage];
+    UIImage* img2 = [info objectForKey:UIImagePickerControllerEditedImage];
+    
+    NSLog(@"%@", [img1 description]);
+    NSLog(@"%@", [img2 description]);
+    
+    _imageView.image = img1;
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)click1:(id)sender {
+    UIImagePickerController* picker = [[UIImagePickerController alloc] init];
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.delegate = self;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (IBAction)click2:(id)sender {
+    // 시뮬레이터에선 동작 안한다.
+    UIImagePickerController* picker = [[UIImagePickerController alloc] init];
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    picker.delegate = self;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
 @end
