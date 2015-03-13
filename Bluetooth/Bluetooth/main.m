c089e3e3921f4b93f6dbe65df3ea5472aa91e579
Hojin Ghim <hojin.ghim@lge.com>
2015-03-13 15:21:12 +0900

    scratch project for bluetooth

---
+++ Bluetooth/Bluetooth/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bluetooth
+//
+//  Created by Eizer-N39 on 2015. 3. 13..
+//  Copyright (c) 2015년 beila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
