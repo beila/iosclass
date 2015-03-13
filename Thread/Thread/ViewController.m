0b06df5555b53472cdc8e5dedf3f12af3aafa637
Hojin Ghim <hojin.ghim@lge.com>
2015-03-13 10:37:53 +0900

    scratch project for thread

---
+++ Thread/Thread/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Thread
+//
+//  Created by Eizer-N39 on 2015. 3. 13..
+//  Copyright (c) 2015년 beila. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    NSLog(@"%s", __func__);
+    [super viewDidLoad];
+}
+
+@end
