9575ebeeb5faad33701959fe600b201b96513182
Hojin Ghim <hojin.ghim@lge.com>
2015-03-13 15:39:22 +0900

    hello bluetooth but just in halfway

--- Bluetooth/Bluetooth/ViewController.m
+++ Bluetooth/Bluetooth/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+- (IBAction)send:(id)sender;
+- (IBAction)connect:(id)sender;
 
 @end
 
@@ -23,5 +26,38 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)send:(id)sender {
+}
+
+- (IBAction)connect:(id)sender {
+    NSLog(@"%s", __func__);
+    GKPeerPickerController* pickerController = [[GKPeerPickerController alloc] init];
+    pickerController.delegate = self;
+    pickerController.connectionTypesMask = GKPeerPickerConnectionTypeNearby;
+    [pickerController show];
+}
+
+#pragma mark - GKPeerPickerControllerDelegate
+
+- (GKSession *)peerPickerController:(GKPeerPickerController *)picker sessionForConnectionType:(GKPeerPickerConnectionType)type {
+    NSLog(@"%s", __func__);
+    
+    GKSession* gkSession = [[GKSession alloc] initWithSessionID:@"TEST"
+                                                    displayName:@"Player1"
+                                                    sessionMode:GKSessionModePeer];
+    return gkSession;
+}
+
+- (void)peerPickerController:(GKPeerPickerController *)picker didConnectPeer:(NSString *)peerID toSession:(GKSession *)session {
+    NSLog(@"%s %@에 연결되었습니다.", __func__, [session displayNameForPeer:peerID]);
+    
+    [picker dismiss];
+    
+    session.delegate = self;
+    [session setDataReceiveHandler:self withContext:nil];
+}
+
+#pragma mark - GKSessionDelegate
 
 @end
