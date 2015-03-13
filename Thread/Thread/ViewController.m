2c7a878c9bffb9081dd84bfca2accdad8ec4ee56
Hojin Ghim <hojin.ghim@lge.com>
2015-03-13 10:47:40 +0900

    hello multi thread

--- Thread/Thread/ViewController.m
+++ Thread/Thread/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+- (IBAction)start:(id)sender;
+- (IBAction)stop:(id)sender;
+- (IBAction)lockTest:(id)sender;
 
 @end
 
@@ -17,6 +21,47 @@
 - (void)viewDidLoad {
     NSLog(@"%s", __func__);
     [super viewDidLoad];
+}
+
+- (IBAction)start:(id)sender {
+}
+
+- (IBAction)stop:(id)sender {
+}
+
+NSLock* lock;
+- (IBAction)lockTest:(id)sender {
+    lock = [[NSLock alloc] init];
+    
+    [NSThread detachNewThreadSelector:@selector(goo) toTarget:self withObject:nil];
+    [NSThread detachNewThreadSelector:@selector(goo) toTarget:self withObject:nil];
+    [NSThread detachNewThreadSelector:@selector(goo) toTarget:self withObject:nil];
+}
+
+int x = 100;
+int y = 100;
+int z = 100;
+- (void)goo{
+    // 더 복잡한 병렬수행이 필요하면 grand central dispatch를 공부해라.
+    
+    @synchronized(self) {
+        x = 100;
+        usleep(5);
+        x = x + 1;
+        NSLog(@"\t%d", x);
+    }
+    
+    [lock lock];
+    y = 100;
+    usleep(5);
+    y = y + 1;
+    [lock unlock];
+    NSLog(@"\t\t%d", y);
+    
+    z = 100;
+    usleep(5);
+    z = z + 1;
+    NSLog(@"\t\t\t%d", z);
 }
 
 @end
