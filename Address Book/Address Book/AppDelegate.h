b135ff8e633d3409ff6e3829dbb2305e24d2197f
Hojin Ghim <hojin.ghim@lge.com>
2015-03-13 12:27:07 +0900

    UI for address book

---
+++ Address Book/Address Book/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Address Book
+//
+//  Created by Eizer-N39 on 2015. 3. 13..
+//  Copyright (c) 2015년 beila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Address Book/Address Book/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Address Book
+//
+//  Created by Eizer-N39 on 2015. 3. 13..
+//  Copyright (c) 2015년 beila. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
